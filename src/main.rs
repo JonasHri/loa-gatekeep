971f9621c8db8857097a5d02dbb2a7825ca6db85
JonasHri <jonasharriehausen@gmail.com>
2024-01-30 17:23:46 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use meter_core::{start_capture, get_most_common_ip};
 fn main() {
     let ip = meter_core::get_most_common_ip().unwrap();
-    let port = 6040;
+    let port = 6020;
     let rx = meter_core::start_capture(ip, port).unwrap();
     while let Ok((op, data)) = rx.recv() {
-        println!("{:?}\n\n{:?}", op, data);
+        println!("op:\n{:?}\n\ndata:\n{:?}", op, data);
     }
-
 }
